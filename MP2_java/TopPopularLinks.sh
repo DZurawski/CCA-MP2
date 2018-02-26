ab2e443b3fd706c1aba7f4a7d8005c70808607de
DZurawski <danrzurawski@gmail.com>
2018-02-26 00:58:09 -0600

    asdf

---
+++ MP2_java/TopPopularLinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "====================="
+rm -rf TopPopularLinksClasses
+mkdir ./TopPopularLinksClasses
+echo "====================="
+javac -cp $(hadoop classpath) TopPopularLinks.java -d TopPopularLinksClasses
+echo "====================="
+jar -cvf TopPopularLinks.jar -C TopPopularLinksClasses/ ./
+echo "====================="
+hadoop jar TopPopularLinks.jar TopPopularLinks dataset/links ./D-output
+echo "====================="
+cat D- output/part-r-00000
